313c818673c429f227d869e39310539bcdb3bc30
Andrest31 <128243532+Andrest31@users.noreply.github.com>
2025-02-14 21:32:33 +0300

    Подняли через контерейнизацию весь проект

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+      interval: 1000,
+  },
     proxy: {
       '/api': 'http://server:5000', // Проксируем запросы к API
     },
